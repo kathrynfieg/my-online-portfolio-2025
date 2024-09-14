05c9d9af82e6138efd9aa264f7ca4ca598148728
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-14 19:49:57 +0930

    feat: create `Blog` view

--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,13 @@ import { createMemoryHistory, createRouter } from 'vue-router';
 import HomeView from '@/views/Home.vue';
 import AboutView from '@/views/About.vue';
 import ProjectsView from '@/views/Projects.vue';
+import BlogView from '@/views/Blog.vue';
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
   { path: '/projects', component: ProjectsView },
+  { path: '/blog', component: BlogView },
 ];
 
 const router = createRouter({
