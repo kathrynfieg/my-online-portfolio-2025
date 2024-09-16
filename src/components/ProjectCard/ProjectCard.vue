6b63258dba37c4ad90267f83dc5925655c77a952
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-16 14:59:55 +0930

    tmp: project page looking good

--- src/components/ProjectCard/ProjectCard.vue
+++ src/components/ProjectCard/ProjectCard.vue
@@ -1,28 +1,50 @@
+<script setup lang="ts">
+import { Chip } from "@/components";
+</script>
+
 <template>
   <div class="project-card">
-    <div class="project-image">IMG</div>
+    <!-- <div class="project-image">IMG</div> -->
+    <div class="me-pic-div"></div>
     <div>
-      <div>Project Name</div>
+      <!-- <data value=""></data> -->
+      <div style="font-weight: 600; font-size: larger">Project Name</div>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </div>
       <br />
-      <div>Tech stack: Vue, typescript, storybook</div>
+      <div>Tech stack:</div>
+      <div style="display: flex; gap: 0.3rem">
+        <Chip label="Vue" />
+        <Chip label="typescript" />
+        <Chip label="storybook" />
+      </div>
+
       <br />
       <div class="buttons">
-        <button>Live Site</button>
-        <button>Code</button>
+        <button>Live demo</button>
+        <button>Source code</button>
       </div>
     </div>
   </div>
+  <br />
+  <hr />
 </template>
 
 <style scoped>
+.me-pic-div {
+  background-image: url("../../assets/me2.JPG");
+  height: 400px;
+  width: 100%;
+  background-size: cover;
+  border-radius: 1rem;
+}
+
 .project-card {
   /* background-color: blueviolet; */
   width: 100%;
-  border: 1px solid;
+
   padding: 1rem;
   display: flex;
   gap: 1rem;
@@ -44,6 +66,23 @@
   width: 8rem;
   background-color: black;
   color: white;
-  font-family: 'IBM Plex Mono', monospace;
+  font-family: "IBM Plex Mono", monospace;
+}
+
+@media (max-width: 768px) {
+  .project-card {
+    /* background-color: blueviolet; */
+    width: 100%;
+
+    padding: 1rem;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .me-pic-div {
+    height: 250px;
+    background-size: cover;
+    border-radius: 1rem;
+  }
 }
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,1 +1,191 @@
-<template>Projects</template>
+<script setup lang="ts">
+import { Chip, ProjectCard } from "@/components";
+</script>
+
+<template>
+  <div class="about-screen container">
+    <p class="header">My Projects<span class="blinking-cursor">|</span></p>
+    <div class="content">
+      <div class="side-header">
+        <h1 style="">
+          Notable (or just fun<box-icon name="smile"></box-icon>) side-projects
+          i've worked on
+        </h1>
+        <br />
+        <a
+          href="https://github.com/kathrynfieg?tab=repositories"
+          target="_blank"
+          style="display: flex; gap: 0.3rem"
+        >
+          <box-icon name="link-external"></box-icon>
+          Feel free to check out more of my projects on GitHub</a
+        >
+      </div>
+      <div class="">
+        <ProjectCard />
+        <br />
+        <ProjectCard /> <br />
+        <ProjectCard /> <br />
+        <ProjectCard /> <br />
+        <ProjectCard /> <br />
+        <ProjectCard /> <br />
+        <ProjectCard />
+        <br />
+        <!-- <p>
+          I'm Kat, a Frontend developer based in Adelaide, Australia and I focus
+          on web applications built with vue.js
+        </p>
+        <p>
+          I'm Kat, a Frontend developer based in Adelaide, Australia and I focus
+          on web applications built with vue.js
+        </p>
+        <p>
+          I'm Kat, a Frontend developer based in Adelaide, Australia and I focus
+          on web applications built with vue.js
+        </p>
+        <p>
+          I'm Kat, a Frontend developer based in Adelaide, Australia and I focus
+          on web applications built with vue.js
+        </p>
+
+        <button>My Projects</button> -->
+      </div>
+    </div>
+    <!-- <div class="me-pic-div"></div> -->
+  </div>
+</template>
+
+<style scoped>
+.about-screen {
+  padding: 3rem 1rem;
+  padding-top: 7rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.blinking-cursor {
+  font-weight: 300;
+  font-size: 2rem;
+  padding-left: 0.5rem;
+  color: #2e3d48;
+  display: inline-block;
+  animation: blink 1s step-end infinite;
+}
+
+.header {
+  font-size: 1.5rem;
+}
+
+h1 {
+  font-size: 3.5rem;
+  line-height: 4rem;
+}
+
+.side-header {
+  max-width: 35%;
+  position: sticky;
+  top: 6rem;
+  height: fit-content;
+}
+
+.short-me {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.me-pic-div {
+  background-image: url("../assets/me2.JPG");
+  height: 400px;
+  width: 100%;
+
+  background-size: cover;
+  border-radius: 1rem;
+}
+
+.tech-stack {
+  display: flex;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+}
+
+.me-pic {
+  /* min-width: 100%;
+  max-height: 500px; */
+  /* background-size: contain; */
+  /* border: 2px solid black; */
+}
+
+.content {
+  display: flex;
+  gap: 5rem;
+}
+
+button {
+  cursor: pointer;
+  border: 2px solid;
+  padding: 0.5rem;
+  width: 10rem;
+  margin-top: 2rem;
+  background-color: white;
+  font-family: "IBM Plex Mono", monospace;
+  align-self: flex-end;
+}
+
+button:hover {
+  background-color: cadetblue;
+  color: white;
+}
+
+@media (max-width: 768px) {
+  .me-pic-div {
+    /* background-image: url('../assets/me2.JPG'); */
+    height: 200px;
+    /* width: 100%; */
+    /* background-size: cover; */
+  }
+
+  .about-screen {
+    padding: 2rem 1rem;
+    padding-top: 6rem;
+  }
+
+  .content {
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  h1 {
+    font-size: 3rem;
+    line-height: 3.5rem;
+    max-width: 100%;
+    width: 100%;
+    position: static;
+    top: auto;
+  }
+
+  .side-header {
+    max-width: 100%;
+    width: 100%;
+    position: static;
+    top: auto;
+  }
+
+  button {
+    align-self: auto;
+    width: 100%;
+  }
+}
+
+@keyframes blink {
+  from,
+  to {
+    color: transparent;
+  }
+  50% {
+    color: black;
+  }
+}
+</style>
