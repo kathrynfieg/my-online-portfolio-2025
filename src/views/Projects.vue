49033305942d159c5d9a988d1e312021047144f8
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-17 15:56:43 +0930

    feat: `Projects` view

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,1 +1,76 @@
-<template>Projects</template>
+<script setup lang="ts">
+import { ProjectCard } from '@/components';
+</script>
+
+<template>
+  <div class="about-screen container">
+    <h1 class="small-header">
+      My Projects<span class="blinking-cursor">|</span>
+    </h1>
+    <div class="flex flex-col gap-4 md:flex-row md:gap-20">
+      <div class="w-full max-w-full h-fit md:max-w-[35%] md:sticky md:top-24">
+        <h1 class="big-header">
+          Notable (or just fun<box-icon name="smile"></box-icon>) side-projects
+          i've worked on
+        </h1>
+        <br />
+        <a
+          href="https://github.com/kathrynfieg?tab=repositories"
+          target="_blank"
+          class="flex gap-1"
+        >
+          <box-icon name="link-external"></box-icon>
+          Feel free to check out more of my projects on GitHub
+        </a>
+      </div>
+      <div class="flex flex-col gap-2">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.about-screen {
+  padding: 3rem 1rem;
+  padding-top: 7rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+button {
+  cursor: pointer;
+  border: 2px solid;
+  padding: 0.5rem;
+  width: 10rem;
+  margin-top: 2rem;
+  background-color: white;
+  font-family: 'IBM Plex Mono', monospace;
+  align-self: flex-end;
+}
+
+button:hover {
+  background-color: cadetblue;
+  color: white;
+}
+
+@media (max-width: 768px) {
+  .about-screen {
+    padding: 2rem 1rem;
+    padding-top: 6rem;
+  }
+
+  button {
+    align-self: auto;
+    width: 100%;
+  }
+}
+</style>
