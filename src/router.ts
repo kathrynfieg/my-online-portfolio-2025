b77ffaab332e9e6c6dd09579664e794c9c062c09
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-17 16:02:07 +0930

    feat: update history mode and scrollBehavior

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,26 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from '@/views/Home.vue';
-import AboutView from '@/views/About.vue';
-import ProjectsView from '@/views/Projects.vue';
-import BlogView from '@/views/Blog.vue';
+import HomeView from "@/views/Home.vue";
+import AboutView from "@/views/About.vue";
+import ProjectsView from "@/views/Projects.vue";
+import BlogView from "@/views/Blog.vue";
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/about', component: AboutView },
-  { path: '/projects', component: ProjectsView },
-  { path: '/blog', component: BlogView },
+  { path: "/", component: HomeView },
+  { path: "/about", component: AboutView },
+  { path: "/projects", component: ProjectsView },
+  { path: "/blog", component: BlogView },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
   routes,
 });
 
