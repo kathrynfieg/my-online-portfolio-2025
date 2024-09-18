5ad46d4a16490c1f13d092bb41a58ab3fc21ba11
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-18 17:24:59 +0930

    feat: add `sm` and `xs` variant

--- src/components/Chip/Chip.vue
+++ src/components/Chip/Chip.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import { PropType } from 'vue';
+
+export type IChipSize = 'default' | 'sm' | 'xs';
+
 defineProps({
-  label: String,
+  label: {
+    label: String,
+    default: '',
+  },
+  size: {
+    type: String as PropType<IChipSize>,
+    default: 'default',
+  },
 });
 </script>
 
 <template>
-  <div class="bg-gray-200 w-fit px-4 py-0.5 rounded-lg">{{ label }}</div>
+  <div
+    :class="[
+      'bg-gray-200 w-fit px-4 py-0.5 rounded-lg',
+      {
+        'text-sm px-3': size === 'sm',
+        'text-xs px-2': size === 'xs',
+      },
+    ]"
+  >
+    {{ label }}
+  </div>
 </template>
