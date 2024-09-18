76c2813aac2f8baf6ea8043c13a981191db0ca44
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-18 17:24:00 +0930

    style: style updates

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/vue3';
+import 'boxicons';
 import '../src/style.css';
 
 const preview: Preview = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#00563E',
+        'primary-orange': '#FF7F01',
+        'primary-yellow': '#FEA501',
+        'primary-gray': '#333333',
+
+        'pastel-blue': '#244856',
+        'pastel-red': '#E64833',
+        'pastel-brown': '#874F41',
+        'pastel-cyan': '#90AEAD',
+        'pastel-cream': '#FBE9D0',
+      },
+    },
   },
   plugins: [],
 };
