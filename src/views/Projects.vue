24379f162d192fc412d5ddac2c92b06895224cb8
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-18 17:34:15 +0930

    feat: add tmp mock projects to projects list

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { ProjectCard } from '@/components';
+
+import photo from '@/assets/me.jpg';
+import handleit from '@/assets/handleit.png';
+import tafebuddy from '@/assets/tafebuddy.png';
 </script>
 
 <template>
@@ -7,56 +11,75 @@ import { ProjectCard } from '@/components';
     <h1 class="small-header">
       My Projects<span class="blinking-cursor">|</span>
     </h1>
-    <div class="flex flex-col gap-4 md:flex-row md:gap-20">
-      <div class="w-full max-w-full h-fit md:max-w-[35%] md:sticky md:top-24">
+
+    <div class="flex flex-col gap-4 lg:flex-row lg:gap-20">
+      <!-- header -->
+      <div
+        class="w-full max-w-full h-fit lg:max-w-[35%] lg:sticky lg:top-24 flex flex-col gap-4"
+      >
         <h1 class="big-header">
           Notable (or just fun<box-icon name="smile"></box-icon>) side-projects
           i've worked on
         </h1>
-        <br />
+        <p>
+          Im a huge fan of Vue 3 and love working with it daily in my Job - But
+          I do plan on playing around with different tech for my side-projects!
+        </p>
+      </div>
+
+      <!-- Project list -->
+      <div class="flex flex-col gap-10">
+        <ProjectCard
+          name="Handleit Cleaning Adelaide"
+          description="Website built for our family-owned cleaning business, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          :tech="['Webflow', 'BookingKoala integration']"
+          liveLink="https://www.handleitcleaning.com.au/"
+          :photo="handleit"
+          :number="1"
+        />
+
+        <ProjectCard
+          name="TafeBuddy"
+          description="TAFE SA's Student Results View (SRV) app. elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          :tech="['C#', 'XAML', 'SQL', 'MySQL']"
+          :photo="tafebuddy"
+          sourceCodeLink="https://github.com/kathrynfieg/TafeBuddy_SRV_App?tab=readme-ov-file#tafebuddy"
+          :actions="[
+            {
+              label: 'Video walkthrough',
+              link: 'https://www.youtube.com/watch?v=MSR6I0BNxks&ab_channel=KathrynFieg',
+            },
+          ]"
+          :number="2"
+        />
+
+        <ProjectCard
+          name="PupSpot Adelaide"
+          description="Website built for our family-owned cleaning business, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          :tech="['Vue', 'Firebase', 'Typescript']"
+          liveLink="https://www.handleitcleaning.com.au/"
+          :photo="photo"
+          :number="3"
+        />
+
+        <ProjectCard
+          name="PupSpot Adelaide"
+          description="Website built for our family-owned cleaning business, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          :tech="['Vue', 'Firebase', 'Typescript']"
+          liveLink="https://www.handleitcleaning.com.au/"
+          :number="4"
+        />
+
+        <!-- See more link -->
         <a
           href="https://github.com/kathrynfieg?tab=repositories"
           target="_blank"
-          class="flex gap-1"
+          class="flex gap-1 underline"
         >
           <box-icon name="link-external"></box-icon>
           Feel free to check out more of my projects on GitHub
         </a>
       </div>
-      <div class="flex flex-col gap-2">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-      </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-button {
-  cursor: pointer;
-  border: 2px solid;
-  padding: 0.5rem;
-  width: 10rem;
-  margin-top: 2rem;
-  background-color: white;
-  font-family: 'IBM Plex Mono', monospace;
-  align-self: flex-end;
-}
-
-button:hover {
-  background-color: cadetblue;
-  color: white;
-}
-
-@media (max-width: 768px) {
-  button {
-    align-self: auto;
-    width: 100%;
-  }
-}
-</style>
