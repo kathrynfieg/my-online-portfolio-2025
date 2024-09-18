49606042dfde4fd1c00f30d3fe036acf579469e4
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-18 22:36:15 +0930

    fix: fix mobile format

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,6 +2,6 @@
   <footer
     class="flex justify-center items-center p-4 text-sm w-full text-center bg-black text-white"
   >
-    <p>kathrynfieg@gmail.com | +61 448 8003 40</p>
+    <p>kathrynfieg@gmail.com | +61 448 800 340</p>
   </footer>
 </template>
