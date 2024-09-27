8bd85cf7cdc792600ffb089e5dc0268ea68c16c7
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-27 16:25:20 +0930

    feat: install and setup `firebase`

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
+import { VueFire } from 'vuefire';
 import App from './App.vue';
 import router from '@/router';
+import { firebaseApp } from './firebase';
 import './style.css';
 import 'boxicons';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueFire, {
+  firebaseApp,
+  modules: [],
+});
 app.mount('#app');
